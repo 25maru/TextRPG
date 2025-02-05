0ad986b81d4f40fac97ecea5ed5a70cb427eea7c
25maru <qkrwldnjs6694@gmail.com>
2025-02-05 15:20:31 +0900

    Fix: 깃 레포지토리 설정 문제 해결
    - 폴더를 옮길 때 프로젝트가 제대로 연결되지 않았던 문제
    - Visual Studio 깃 이그노어 파일 추가

--- TextRPG/obj/Debug/net6.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/obj/Debug/net6.0/TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9708cfb29fb58d6b70ca6bde25a4b2f5b7aa80bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
