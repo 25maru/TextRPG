33864057abea86e7cc8f563dfb9870e1e8d5a116
boyoun-gg <duki9115@naver.com>
2025-02-06 20:46:16 +0900

    Feat: 전투 종료 후 UI

--- TextRPG/BattleEndUI.cs
+++ TextRPG/BattleEndUI.cs
@@ -1,7 +1,30 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 // 최보윤님
 public class BattleEndUI
 {
-    //눈이 많이 와요
+    /// <summary>
+    /// 전투에 붙여주세요.
+    /// </summary>
+    /// <param name="isWin"></param>
+    /// <param name="monsters"></param>
+    public static void BattleEnd(bool isWin, List<Monster> monsters)
+    {
+        if (isWin) GiveReward(monsters);
+        else Lose();
+    }
+    private static void GiveReward(List<Monster> monsters)
+    {
+        Console.WriteLine("전투를 승리하였습니다");
+
+        for (int i = 0; i < monsters.Count; i++)
+        {
+            Console.WriteLine($"처치한 몬스터:{monsters[i]}");
+        }
+    }
+    private static void Lose()
+    {
+        Console.WriteLine("전투에서 패배하셨습니다. 게임오버");
+    }
 }
