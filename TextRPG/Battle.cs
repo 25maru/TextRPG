de65dc0e46aba1917290ce971a1e22c1f7d055af
EEL_B <sang980203@gmail.com>
2025-02-07 21:53:34 +0900

    fix:  전투 버그 수정
    1. Battle중 전체 몬스터의 리스트가 사망한 몬스터 만큼 줄어들어 표시되지 않는 버그 해결.
    2. Battle 중 IsDead가 true인 개체에게 공격 시, 해당 개체의 Health가 음수로 전환되는 버그 해결.
    2-1. IsDead 개체 선택 시 GameManeger ErrorText 호출과 함께 플레이어턴으로 넘어가도록 변경.

--- TextRPG/Battle.cs
+++ TextRPG/Battle.cs
@@ -9,7 +9,6 @@ public class Battle
     public List<Item> _rewarditem;
 
     GameManager GM = GameManager.Instance;
-
     /// <summary>
     /// 캐릭터, 몬스터 리스트, 리워드 리스트
     /// </summary>
@@ -25,9 +24,9 @@ public class Battle
         GM.ShowHeader("전투 시작 !!!", "몬스터와의 전투를 시작합니다.");
 
         while ((!_character.IsDead) && (isAliveMonstersExists(_monsters))) // 몬스터 사망처리
-        { 
+        {
             PlayerTurn();
-            for (int i = 0; i < _monsters.Count(); i++) //살아있는 몬스터 만큼 턴을 씀 
+            for (int i = 0; i < _monsters.Count(); i++) //몬스터 만큼 턴을 씀 
             {
                 if (_monsters[i].IsDead) //번호 몬스터가 죽어있으면
                 {
@@ -35,7 +34,7 @@ public class Battle
                 }
                 else //살아있으면
                 {
-                    MonsterTurn(_monsters[i], _character); 
+                    MonsterTurn(_monsters[i], _character);
                 }
             }
         }
@@ -52,9 +51,7 @@ public class Battle
 
     public bool isAliveMonstersExists(List<Monster> _monsters) // 살아있는 몬스터 존재여부
     {
-
-        Console.WriteLine($"살아있는 몬스터가 {CountAliveMonsters(_monsters)} 마리 있음.");
-
+        Console.WriteLine($"남은 몬스터: {CountAliveMonsters(_monsters)}/{_monsters.Count()}.");
         return _monsters.Exists(_monsters => !_monsters.IsDead);
     }
 
@@ -69,50 +66,67 @@ public class Battle
 
     public void PlayerTurn() //플레이어의 턴
     {
-
-        foreach (var monster in _monsters)
+        for (int i = 0; i < _monsters.Count; i++) //몬스터 현재 상태 보여주기
         {
-
-            //Console.WriteLine("플레이어 턴 메소드 입니다.");
+            _monsters[i].ShowMonster(i + 1);
         }
-        for (int i = 0; i < CountAliveMonsters(_monsters); i++) //몬스터 현재 상태 보여주기
-                {
-                    _monsters[i].ShowMonster(i+1);
-                }
         Console.WriteLine();
 
         GM.OptionText(1, "공격");
         GM.OptionText(2, "회복 포션 사용");
         GM.OptionText(3, "공격 포션 사용");
+        GM.OptionText(4, "던전 클리어");
+        GM.OptionText(5, "던전 실패");
 
-        int KeyCode = GM.GetInput(1, 3);
+        int KeyCode = GM.GetInput(1, 6);
         switch (KeyCode)
         {
             case 1:
                 GM.ShowHeader("공격을 가합니다", "몬스터를 선택해주세요.");
 
-                for (int i = 0; i < CountAliveMonsters(_monsters); i++) //몬스터 현재 상태 보여주기
+                for (int i = 0; i < _monsters.Count; i++) //몬스터 현재 상태 보여주기
                 {
                     _monsters[i].ShowMonster(i + 1);
                 }
-                int _TargetMonster = GM.GetInput(0, _monsters.Count+1);
-                PlayerAttack(_character, _monsters[_TargetMonster - 1], (int)_character.TotalAttack);
+                int _TargetMonster = GM.GetInput(0, _monsters.Count + 1);
+
+                if (_monsters[_TargetMonster - 1].IsDead)
+                {
+                    GM.ErrorText($"선택하신 {_monsters[_TargetMonster - 1].Name}은 선택할 수 없는 대상입니다.\n유효한 대상을 선택해주세요.\n");
+                    PlayerTurn();
+                }
+                else
+                {
+                    PlayerAttack(_character, _monsters[_TargetMonster - 1], (int)_character.TotalAttack);
+                }
                 break;
 
             case 2:
                 Console.WriteLine("회복 포션을 사용합니다.");
-                ClearDungeon();
-                break;
+                _character.Health += 20;
+                if (_character.Health >= _character.MaxHealth)
+                {
 
+                }
+                break;
 
             case 3:
                 Console.WriteLine("강화 포션을 사용합니다.");
-                _character.Health = 0;
+                GM.ErrorText("미구현 항목입니다.");
                 break;
 
             case 4:
-                Console.WriteLine("4번 항목입니다.");
+                GM.ShowHeader("던전 클리어를 선택하셨습니다.", "던전 성공 메서드를 호출합니다.");
+                ClearDungeon();
                 break;
+            case 5:
+                GM.ShowHeader("던전 실패를 선택하셨습니다", "던전 실패 메서드를 호출합니다.");
+                FailedDungeon();
+                break;
+            default:
+                Console.WriteLine("어케 띄웠음 이걸");
+                break;
+
         }
     }
 
@@ -140,18 +154,16 @@ public class Battle
             _character.IsDead = true;
             FailedDungeon();
             Console.WriteLine($"{_character.Name}가 죽었습니다. 마을로 돌아갑니다.");
-            //ExitDungeon();
         }
     }
 
-    public void FailedDungeon()
+    public void FailedDungeon() //던전 실패
     {
         BattleEndUI.BattleEnd(false, _monsters);
-        // 마을 복귀 메서드
         GM.ShowMainMenu();
     }
 
-    public void ClearDungeon() ///던전 종료 메서드 //보윤님 코드 우선
+    public void ClearDungeon() //던전 클리어 //보윤님 코드 연결
     {
         BattleEndUI.BattleEnd(true, _monsters);
         GM.ShowMainMenu();
