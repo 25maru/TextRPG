598796534902513caedffe9c96f93c0ea10363d2
boyoun-gg <duki9115@naver.com>
2025-02-12 17:30:43 +0900

    Chore: 불필요한 주석 삭제

--- TextRPG/BattleEndUI.cs
+++ TextRPG/BattleEndUI.cs
@@ -39,10 +39,7 @@ public class BattleEndUI
                 Console.WriteLine("잘못된 입력입니다.");
                 break;
         }
-       /* for (int i = 0; i < monsters.Count; i++)
-        {
-            Console.WriteLine($"처치한 몬스터:{monsters[i]}");
-        }*/
+
         foreach (Monster monster in monsters) 
         {
             Console.WriteLine($"처치한 몬스터:{monster.Name}");
